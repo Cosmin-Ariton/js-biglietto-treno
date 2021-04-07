1ac1c33dae444f2ee2feca506e19fcff40f8ae1a
Cosmin-Ariton <cosminiris12@gmail.com>
2021-04-07 18:17:51 +0200

    js-second

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
-var km_Percorrere = prompt('Quanti km vuoi percorrere?')
-var età_utente = prompt('Quanti anni hai?');
+var km_Percorrere = parseFloat(prompt('Quanti km vuoi percorrere?'))
+var età_utente = parseFloat(prompt('Quanti anni hai?'));
 var biglietto = 0.21;
 var s_minorenne = parseFloat(km_Percorrere * biglietto) * 20 / 100;
 var s_anziano = parseFloat(km_Percorrere * biglietto) * 40 / 100;
 var tot = parseFloat(km_Percorrere * biglietto);
 
 if (età_utente < 18) {
-    console.log('minorenne')
-} else if (età_utente > 65) {
-    console.log('anziano')
+    console.log(tot - s_minorenne)
+} else if (età_utente >= 18 && età_utente <= 65) {
+    console.log(tot)
+} else {
+    console.log(tot - s_anziano)
 }
+
+document.getElementById('conto').innerHTML = 'Il prezzo del biglietto è di ' + parseFloat(tot - s_minorenne) + ' €';
+document.getElementById('conto').innerHTML = 'Il prezzo del biglietto è di ' + parseFloat(tot) + ' €';
+document.getElementById('conto').innerHTML = 'Il prezzo del biglietto è di ' + parseFloat(tot - s_anziano) + ' €';
