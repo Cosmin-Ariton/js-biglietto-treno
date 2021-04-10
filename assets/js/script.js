e10eeff4a3b53208c7ae1f1299de22e88a4ec34d
Cosmin-Ariton <cosminiris12@gmail.com>
2021-04-10 11:06:08 +0200

    js-third

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,19 @@
-var km_Percorrere = parseFloat(prompt('Quanti km vuoi percorrere?'))
-var età_utente = parseFloat(prompt('Quanti anni hai?'));
-var biglietto = 0.21;
-var s_minorenne = parseFloat(km_Percorrere * biglietto) * 20 / 100;
-var s_anziano = parseFloat(km_Percorrere * biglietto) * 40 / 100;
-var tot = parseFloat(km_Percorrere * biglietto);
+var age = prompt("Inserisci la tua età");
+var distance = prompt("Inserisci i km da percorerre");
+var totalPrice = distance * 0.21;
+var discountPrice;
+var msg = "Il prezzo del biglietto è di € " + totalPrice.toFixed(2);
+var discountMsg = "";
 
-if (età_utente < 18) {
-    console.log(tot - s_minorenne)
-} else if (età_utente >= 18 && età_utente <= 65) {
-    console.log(tot)
-} else {
-    console.log(tot - s_anziano)
+if (age < 18) {
+    discountPrice = totalPrice - totalPrice * 0.2;
+    discountMsg = "<br>Hai lo sconto minorenni del 20%!<br>Paghi solo €" + discountPrice.toFixed(2);
+    console.log(discountPrice);
+}
+if (age > 65) {
+    discountPrice = totalPrice - totalPrice * 0.4;
+    discountMsg = "<br>Hai lo sconto senior del 40%!<br>Paghi solo €" + discountPrice.toFixed(2);
+    console.log(discountPrice);
 }
 
-document.getElementById('conto').innerHTML = 'Il prezzo del biglietto è di ' + parseFloat(tot - s_minorenne) + ' €';
-document.getElementById('conto').innerHTML = 'Il prezzo del biglietto è di ' + parseFloat(tot) + ' €';
-document.getElementById('conto').innerHTML = 'Il prezzo del biglietto è di ' + parseFloat(tot - s_anziano) + ' €';
+document.getElementById("output").innerHTML = msg + discountMsg;
